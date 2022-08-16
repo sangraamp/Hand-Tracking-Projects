a17c1292f825828c1d399491c01572763d8dee00
Sangraam Patwardhan <sangraampatwardhan@Sangraams-MacBook-Pro.local>
2022-08-17 02:24:44 +0530

    Init commit

---
+++ HandTrackingMin.py
@@ -0,0 +1,33 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+prevTime = 0
+curTime = 0
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                H, W, C = img.shape
+                cx, cy = int(lm.x * W), int(lm.y * H)
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    curTime = time.time()
+    fps = 1/(curTime - prevTime)
+    prevTime = curTime
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
+    cv2.imshow('Image', img)
+    cv2.waitKey(1)
